65601e28ddb916d0cfb238e1392226759bcb1f29
seremwen <seremwe.81@gmail.com>
2024-02-28 18:49:21 +0000

    project setup

---
+++ src/main/java/zw/nseremwe/SpringBootKeycloackIntegrationApplication.java
@@ -0,0 +1,13 @@
+package zw.nseremwe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKeycloackIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKeycloackIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zw/nseremwe/SpringBootKeycloackIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package zw.nseremwe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKeycloackIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
